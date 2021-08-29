328861ffecaf08295cc485d9edc33351b8fda762
haytaylan <hakan.aytaylan@gmail.com>
2021-08-29 19:24:21 +0300

    Chapter 22

---
+++ src/screens/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Text, View, ScrollView, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+// import screen components
+import Feed from './feed';
+import Favorites from './favorites';
+import MyNotes from './mynotes';
+import NoteScreen from './note';
+
+// navigation stack
+const FeedStack = createStackNavigator({
+    Feed: Feed,
+    Note: NoteScreen
+});
+
+const MyStack = createStackNavigator({
+    MyNotes: MyNotes,
+    Note: NoteScreen
+});
+
+const FavStack = createStackNavigator({
+    Favorites: Favorites,
+    Note: NoteScreen
+});
+
+// navigation tabs
+const TabNavigator = createBottomTabNavigator({
+    FeedScreen: {
+        screen: FeedStack,
+        navigationOptions: {
+            tabBarLabel: 'Feed',
+            tabBarIcon: ({ tintColor }) => (
+                <MaterialCommunityIcons name="home" size={24} color={tintColor} />
+            )
+        }
+    },
+    MyNoteScreen: {
+        screen: MyStack,
+        navigationOptions: {
+            tabBarLabel: 'My Notes',
+            tabBarIcon: ({ tintColor }) => (
+                <MaterialCommunityIcons name="notebook" size={24} color={tintColor} />
+            )
+        }
+    },
+    FavoriteScreen: {
+        screen: FavStack,
+        navigationOptions: {
+            tabBarLabel: 'Favorites',
+            tabBarIcon: ({ tintColor }) => (
+                <MaterialCommunityIcons name="star" size={24} color={tintColor} />
+            )
+        }
+    }
+});
+
+// create the app container
+export default createAppContainer(TabNavigator);
